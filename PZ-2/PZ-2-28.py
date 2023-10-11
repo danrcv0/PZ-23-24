a793d34ea095495cb44c4c5dbf0a69b207a7b2e5
Даня 3 <129081567+danrcv0@users.noreply.github.com>
2023-10-11 18:32:08 +0300

    Update PZ-2-28.py

--- PZ-2/PZ-2-28.py
+++ PZ-2/PZ-2-28.py
@@ -1,12 +1,14 @@
 
 
-def Main(N=input('кол-во секунд: ')):
+def Main():
     while True:
+        N=input('кол-во секунд: ')
         try:
             print(f'Кол-во минут, прошедших с начала последнего часа: {int((int(N)/60)%60)}')
             break
         except:
             print('Что-то пошло не так, попробуйте еще раз!')
+            main()
 
 if __name__ == "__main__":
     Main()
