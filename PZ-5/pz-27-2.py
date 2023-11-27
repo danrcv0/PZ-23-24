441c27e4340534ce7eee63fa618dd29b8f8a3df1
Даня 3 <129081567+danrcv0@users.noreply.github.com>
2023-11-27 22:42:03 +0300

    Update pz-27-2.py

--- PZ-5/pz-27-2.py
+++ PZ-5/pz-27-2.py
@@ -3,18 +3,20 @@
 С помощью этой функции последовательно добавить к данному числу К справа данные цифры D1 и D2, выводя результат каждого добавления."""
 
 
-
-def main(D, K):
+def AddRightDigit(D, K):
     try:
-        if int(D) > 0 or int(D) < 0:
-            K = int(K)
-            K+=int(D)
-            print(K)
-            D = input('Введите число D2: ')
-            K+=int(D)
-            print(K)
+        for x in range(2):
+            if int(D) > 0 and int(D) < 9:
+                    K = f"{K}{D}"
+                    print(K)
+                    return K
+            else:
+                print('Число D не соответствует указанным параметрам, попробуйте заново!')
+                AddRightDigit(D=input('Введите число D: '), K=input('Введите число K: '))
+
     except:
         print('Какое-то число было введено неверно, попробуйте заново')
-        main(D=input('Введите число D: '), K=input('Введите число K: '))
-        
-main(D=input('Введите число D: '), K=input('Введите число K: '))
+        AddRightDigit(D=input('Введите число D: '), K=input('Введите число K: '))
+
+L = AddRightDigit(D=input('Введите число D: '), K=input('Введите число K: '))
+AddRightDigit(D=input('Введите число D: '), K=L)
