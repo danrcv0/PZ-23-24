a328e76d853e35b68abb879f78469b7090af78f0
Даня 3 <129081567+danrcv0@users.noreply.github.com>
2023-12-06 12:31:28 +0300

    Add files via upload

---
+++ PZ-7/PZ-7-1.py
@@ -0,0 +1,22 @@
+
+# 1. Дан символ C и строка S. Удвоить каждое вхождение символа C в строку S.
+
+def main(C,S):
+    try:
+        temp_list = []
+        for char in S:
+            if not char == C:
+                temp_list.append(char)
+            else:
+                temp_list.append(char*2)
+
+        our_str = ''.join(temp_list)
+        return our_str
+
+    except:
+        print('Что-то пошло не так, попробуйте еще раз!')
+        print(main(C=input('Введите C: '), S=input('Введите S: ')))
+
+
+if __name__ == '__main__':
+    print(main(C=input('Введите C: '), S=input('Введите S: ')))
