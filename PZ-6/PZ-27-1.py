bed5b36a5f99793ff1e9064b8f7b1b4250b52a0f
Даня 3 <129081567+danrcv0@users.noreply.github.com>
2023-12-22 10:18:48 +0300

    Update PZ-27-1.py

--- PZ-6/PZ-27-1.py
+++ PZ-6/PZ-27-1.py
@@ -4,14 +4,26 @@
 
 
 
+import random
 
-def main(A):
+
+def main(Q):
+    if Q == 'y':
+        A = []
+        for x in range(random.randint(5,50)):
+            A.append(random.randint(1,100))
+        print(f'Список А: {A}')
+
+    if Q == 'n':
+        A = input('Введите список числе  через пробел: ')
         A = A.split(' ')
-        lst_result = []
-        count = 0
-        for x in range(int(len(A)/2)):
-            lst_result.append(A[count])
-            count+=2
-        print(f'\nсписок с числами четных номеров -- {lst_result}\nминимальный элемент списка -- {min(lst_result)}')
+        print(A)
+    lst_result = []
+    count = 0
+    for x in range(int(len(A)/2)):
+        lst_result.append(A[count])
+        count+=2
+    lst_result.pop(0)
+    print(f'\nсписок с числами четных номеров -- {lst_result}\nминимальный элемент списка -- {min(lst_result)}')
 
-main(A=input('Введите список чисел через пробел: '))
+main(Q=input('Хотите ли вы ввести список ранодомно? (y/n): '))
