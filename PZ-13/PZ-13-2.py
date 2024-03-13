ddea414c95323fae9e042d65a2ee0ae685a103d9
Даня 3 <129081567+danrcv0@users.noreply.github.com>
2024-03-13 14:25:43 +0300

    Update PZ-13-2.py

--- PZ-13/PZ-13-2.py
+++ PZ-13/PZ-13-2.py
@@ -1,14 +1,15 @@
 
-
 """2. В матрице элементы строки N (N задать с клавиатуры) увеличить на 3."""
 
 
 
+import random
 def Main():
-    matrix = [[int(input('Введите первое эелемент: ')), int(input('Введите второй эелемент: ')), int(input('Введите третий эелемент: '))]]
-    print(matrix)
-    matrix[0] = [i+3 for i in matrix[0]]
-    print(matrix)
+    matrix = [[random.randint(-2, 2) for x in range(3)] for x in range(3)]
+    print(f'{matrix[0]}\n{matrix[1]}\n{matrix[2]}')
+    elem = int(input('Введите номер строки: '))
+    matrix[0] = [i+3 for i in matrix[elem-1]]
+    print(matrix[elem-1])
 
 if __name__ =="__main__":
     Main()
